8d9fddcdbc19334b96f46a0d69b472f9a15a8edd
hemanth120 <147231177+hemanth120@users.noreply.github.com>
2024-12-21 13:34:32 +0530

    server.js

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('/data', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server at http://localhost:${port}`);
 });
